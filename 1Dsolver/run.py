7523147e0488bce756478c95b02170d0616c6d32
Benjamin Maier <maierbn@gmail.com>
2017-05-22 12:09:29 +0200

    Add test case for measuring performance of 1D solvers

---
+++ 1Dsolver/run.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import numpy as np
+#import matplotlib.pyplot as plt
+import subprocess
+import datetime
+import time
+import os
+
+# set environment variable
+os.environ['OPENCMISS_SCE_FILE'] = '1Dsolver.sce'
+
+def check_exit():
+  
+  return    # disable time check
+  
+  now = datetime.datetime.now()
+  if now.hour >= 7 and now.hour < 20:  # wait if 7 <= hour <= 20
+    print "Don't run program because it is between 7 and 20."
+    time.sleep(60*60)
+  else:
+    print "OK"
+
+def run(xi1,ode,msolver,mprecond):   
+  print "xi1={0}".format(int(xi1))
+  command = "$OPENCMISS_REL_DIR/cuboid $OPENCMISS_SCE_FILE $OPENCMISS_INPUT_DIR xi1={}, ODESolverId={}, MonodomainSolverId={}, MonodomainPreconditionerId={}"\
+  .format(int(xi1), int(ode), int(msolver), int(mprecond))
+
+  #print command
+  try:
+    subprocess.check_call(command, shell=True)
+  except:
+    pass
+
+
+n_start = 1   # size of smallest problem to begin with
+last_total = 0
+f = 1
+a = 1
+ode = 1       # 1 explicit Euler, 2 BDF
+msolver = 1   # 1 SOLVER_DIRECT_LU, 2 SOLVER_ITERATIVE_GMRES, 3 SOLVER_ITERATIVE_CONJUGATE_GRADIENT, 4 SOLVER_ITERATIVE_CONJGRAD_SQUARED
+precond = 1   # 1 NO_PRECONDITIONER, 2 JACOBI_PRECONDITIONER, 3 BLOCK_JACOBI_PRECONDITIONER, 4 SOR_PRECONDITIONER, 5 INCOMPLETE_CHOLESKY_PRECONDITIONER, 6 INCOMPLETE_LU_PRECONDITIONER, 7 ADDITIVE_SCHWARZ_PRECONDITIONER
+
+for n in range(n_start,1000):
+  xi1 = np.round(n**(1.2))
+  
+  # CG
+  precond = 1
+  for msolver in [1, 2, 4]:
+    check_exit()
+    run(xi1,ode,msolver,precond)
+
+  # CG with different preconditioners
+  msolver = 3
+  
+  for precond in range(1,8):
+    check_exit()
+    run(xi1,ode,msolver,precond)
+
+  
