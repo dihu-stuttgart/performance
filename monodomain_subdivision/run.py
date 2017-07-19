a681b48856081cbc80de21607e09d91637eb7354
Benjamin Maier <maierbn@gmail.com>
2017-07-19 18:00:54 +0200

    Improve run script

--- monodomain_subdivision/run.py
+++ monodomain_subdivision/run.py
@@ -31,10 +31,30 @@ def run(p,x,y,z,xi1,ax,ay,az,ode,msolver,mprecond):
 
   #print command; return
   
+  print command
+
   # execute command
   try:
-    subprocess.check_call(command, shell=True)
-  except:
+    with open('log.txt','ab') as log:
+      log.write("\n\n\n-------- new command ------------------------------------------\n")
+      log.write(command+"\n")
+      log.write("start: "+datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")+"\n")
+
+    output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+
+    with open('log.txt','ab') as log:
+      log.write("end:   "+datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")+"\n\n")
+      log.write(output+"\n")
+  except subprocess.CalledProcessError as exc:
+    with open('log.txt', 'ab') as log:
+      log.write('Command failed, return code: '+str(exc.returncode)+"\n")
+      log.write(exc.output+"\n\n")
+      log.write(output)
+  else:
+    with open('log.txt', 'ab') as log:
+      log.write("Command failed\n")
+      log.write(output)
+
     pass
 
 n_start = 1   # size of smallest problem to begin with
