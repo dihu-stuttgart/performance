078a4295482d8ad1c8e9799930560538043e92dc
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2019-03-03 18:08:14 +0100

    run

--- opendihu/weak_scaling/run.py
+++ opendihu/weak_scaling/run.py
@@ -14,6 +14,12 @@ n_available_nodes = 10000000
 if len(sys.argv) > 1:
   n_available_nodes = (int)(sys.argv[1])
 
+emg_solver_type = None
+if len(sys.argv) > 2:
+  emg_solver_type = sys.argv[2]
+
+print("n_available_nodes: {}".format(n_available_nodes))
+
 """
 7x7fibers.bin                49
 15x15fibers.bin             225
@@ -28,33 +34,42 @@ if len(sys.argv) > 1:
 """
 
 def run(x,y,z,n_fibers_per_dimension):
-  
+  global emg_solver_type 
   opendihu_home = os.environ.get("OPENDIHU_HOME")
   if opendihu_home == "":
     print("Error! $OPENDIHU_HOME is not set!")
     return
     
+  pes_per_node = min(24,x*y*z)
   example_home = os.path.join(opendihu_home, "examples/electrophysiology/fibers_emg")
   settings_file = os.path.join(example_home, "settings_fibers_emg.py")
 
-  scenario_name = "weak_scaling_{}_{}_{}".format(x,y,z)
+  scenario_name = "weak_scaling_lu_{}_{}_{}".format(x,y,z)
 
   fiber_file = "../../input/{n}x{n}fibers.bin".format(n=n_fibers_per_dimension)
+  #firing_times_file = "../../input/MU_firing_times_immediately.txt"
+  firing_times_file = "../../input/MU_firing_times_real.txt"
+  if emg_solver_type is None:
+    emg_solver_type = "gmres"
+  else:
+    scenario_name += "_"
 
-  command = "aprun \
-  --cpu-binding $({OPENDIHU_HOME}/scripts/generate_cpu_list.py {X} {Y} {Z}) \
-  --pes-per-node 24 \
+  command = "export MPICH_RANK_REORDER_METHOD=3; echo $({OPENDIHU_HOME}/scripts/generate_cpu_list.py {X} {Y} {Z}) > MPICH_RANK_ORDER; echo '{n_available_nodes} available compute nodes';\
+  aprun \
+  --pes-per-node {N} \
   -n {NP} \
   {EXAMPLE_HOME}/build_release/fibers_emg {SETTINGS_FILE} \
     --scenario_name {SCENARIO_NAME} \
     --n_subdomains {X} {Y} {Z} \
     --fiber_file {FIBER_FILE} \
-    --end_time 1.0 ".format(OPENDIHU_HOME=opendihu_home, X=x, Y=y, Z=z, NP=x*y*z, EXAMPLE_HOME=example_home, SETTINGS_FILE=settings_file, SCENARIO_NAME=scenario_name, FIBER_FILE=fiber_file)
+    --emg_solver_type {EMG_SOLVER_TYPE} \
+    --firing_times_file {FIRING_TIMES_FILE} \
+    --end_time 1.0 ".format(OPENDIHU_HOME=opendihu_home, X=x, Y=y, Z=z, NP=x*y*z, N=pes_per_node, EXAMPLE_HOME=example_home, SETTINGS_FILE=settings_file, SCENARIO_NAME=scenario_name, FIBER_FILE=fiber_file, n_available_nodes=n_available_nodes, FIRING_TIMES_FILE=firing_times_file, EMG_SOLVER_TYPE=emg_solver_type)
 
   print("partitioning {:2d}*{:2d}*{:2d}={:5d}  {:3d}^2={:6d} fibers, fibers/rank: {:5f}, need {:4d} nodes".format(x,y,z,x*y*z, n_fibers_per_dimension, n_fibers_per_dimension**2, float(n_fibers_per_dimension**2)/(x*y*z), int(np.ceil((x*y*z)/24.))))
-  return
+  #return
 
-  print(command); return 
+  #print(command); return 
   print(command)
 
   # execute command
@@ -77,6 +92,12 @@ def run(x,y,z,n_fibers_per_dimension):
 
     pass
 
+import datetime
+now = datetime.datetime.now()
+print(" ======= run.py =======") 
+print(now.strftime("%d/%m/%Y %H:%M:%S"))
+
+# old
 partitionings = [
   [2,  2,  1,    7],
   [3,  3,  2,   15],
@@ -89,16 +110,39 @@ partitionings = [
   [34, 34, 24, 533],
 ]
 
+# new
+partitionings = [ 
+  [2,  2,  1,   7], 
+  [3,  3,  2,  13],
+  [4,  4,  4,  25],
+  [5,  5,  6,  37],
+  [7,  8,  8,  67], 
+  [10, 10, 12, 109],
+  [17, 17, 12, 187],
+  [18, 18, 24, 277],
+  [27, 27, 24, 427],
+  [34, 34, 24, 523],
+]
+
+
+
+#partitioning = partitionings[1]
+#run(partitioning[0], partitioning[1], partitioning[2], partitioning[3])
+#quit()
+
 for partitioning in partitionings:
   
   n_ranks = partitioning[0] * partitioning[1] * partitioning[2]
   n_nodes = int(np.ceil(n_ranks/24.))
-  
+ 
+  if n_available_nodes > 3 and n_nodes < n_available_nodes:
+    continue 
   if n_nodes > n_available_nodes:
     break
   run(partitioning[0], partitioning[1], partitioning[2], partitioning[3])
   
 """
+old: 
 partitioning  2* 2* 1=    4    7^2=    49 fibers, fibers/rank: 12.250000, need    1 nodes
 partitioning  3* 3* 2=   18   15^2=   225 fibers, fibers/rank: 12.500000, need    1 nodes
 partitioning  4* 5* 4=   80   29^2=   841 fibers, fibers/rank: 10.512500, need    4 nodes
@@ -109,5 +153,16 @@ partitioning 25*25*24=15000  393^2=154449 fibers, fibers/rank: 10.296600, need  
 partitioning 27*27*24=17496  435^2=189225 fibers, fibers/rank: 10.815329, need  729 nodes
 partitioning 34*34*24=27744  533^2=284089 fibers, fibers/rank: 10.239655, need 1156 nodes
 
+new:
+partitioning  2* 2* 1=    4    7^2=    49 fibers, fibers/rank: 12.250000, need    1 nodes
+partitioning  3* 3* 2=   18   13^2=   169 fibers, fibers/rank: 9.388889, need    1 nodes
+partitioning  4* 4* 4=   64   25^2=   625 fibers, fibers/rank: 9.765625, need    3 nodes
+partitioning  5* 5* 6=  150   37^2=  1369 fibers, fibers/rank: 9.126667, need    7 nodes
+partitioning  7* 8* 8=  448   67^2=  4489 fibers, fibers/rank: 10.020089, need   19 nodes
+partitioning 10*10*12= 1200  109^2= 11881 fibers, fibers/rank: 9.900833, need   50 nodes
+partitioning 17*17*12= 3468  187^2= 34969 fibers, fibers/rank: 10.083333, need  145 nodes
+partitioning 18*18*24= 7776  277^2= 76729 fibers, fibers/rank: 9.867413, need  324 nodes
+partitioning 27*27*24=17496  427^2=182329 fibers, fibers/rank: 10.421182, need  729 nodes
+partitioning 34*34*24=27744  523^2=273529 fibers, fibers/rank: 9.859033, need 1156 nodes
 
 """
