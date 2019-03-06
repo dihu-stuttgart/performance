3c376a5c57a6fc253587a97009bf60068c785194
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2019-03-06 18:50:00 +0100

    uiadenfg

---
+++ opendihu/weak_scaling/sub_no_rr.sh
@@ -0,0 +1,7 @@
+qsub no_rank_reordering_145.pbs
+qsub no_rank_reordering_19.pbs
+qsub no_rank_reordering_324.pbs
+qsub no_rank_reordering_3.pbs
+qsub no_rank_reordering_50.pbs
+qsub no_rank_reordering_7.pbs
+
