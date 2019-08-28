2197acc3775fcc2721f34538256167e32e17ab96
Benjamin Maier <maierbn@gmail.com>
2019-08-28 18:00:43 +0200

    udptae result

--- opendihu/02_propagation_velocity/run.sh
+++ opendihu/02_propagation_velocity/run.sh
@@ -5,7 +5,7 @@
 # <n_processes_per_fiber> <n_fibers> <n_nodes_per_fiber> <scenario_name>
 
 #for n_nodes in $(seq 20 20 600); do
-for n_nodes in $(seq 20 120 240); do
+for n_nodes in $(seq 60 20 240); do
 
 # shorten model
 rm -rf out
