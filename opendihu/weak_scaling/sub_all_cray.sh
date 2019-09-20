fa7400f35f17601d57146cc141e80e694269010c
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2019-09-20 12:51:02 +0200

    add scripts

---
+++ opendihu/weak_scaling/sub_all_cray.sh
@@ -0,0 +1,11 @@
+#qsub new_cray_1156.pbs
+#qsub new_cray_729.pbs
+#qsub new_cray_324.pbs
+qsub new_cray_145.pbs
+qsub new_cray_50.pbs
+qsub new_cray_19.pbs
+qsub new_cray_7.pbs
+qsub new_cray_3.pbs
+#qsub new_gnu_7.pbs
+#qsub new_gnu_3.pbs
+
---
+++ opendihu/weak_scaling/sub_all_gnu.sh
@@ -0,0 +1,9 @@
+#qsub new_gnu_1156.pbs
+qsub new_gnu_729.pbs
+qsub new_gnu_324.pbs
+#qsub new_gnu_145.pbs
+#qsub new_gnu_50.pbs
+#qsub new_gnu_19.pbs
+#qsub new_gnu_7.pbs
+#qsub new_gnu_3.pbs
+
