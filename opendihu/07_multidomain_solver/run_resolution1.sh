ae291ec4d19e4b03e0e1a6924019faeaec1d4e2a
Benjamin Maier <maierbn@gmail.com>
2021-01-07 08:50:51 +0100

    update

--- opendihu/07_multidomain_solver/run_resolution1.sh
+++ opendihu/07_multidomain_solver/run_resolution1.sh
@@ -1,8 +1,8 @@
-example_dir=$OPENDIHU_HOME/examples/electrophysiology/multidomain_with_fat
+example_dir=$OPENDIHU_HOME/examples/electrophysiology/multidomain/multidomain_with_fat
 
 killall -9 multidomain_shorten_with_fat
 
-for preconditioner in boomeramg euclid bjacobi sor pilut none; do
+for preconditioner in euclid bjacobi sor pilut none; do
 #for preconditioner in boomeramg; do
 #for preconditioner in bjacobi sor; do
 #for preconditioner in bjacobi; do
@@ -21,13 +21,13 @@ $example_dir/build_release/multidomain_shorten_with_fat \
 $example_dir/settings_multidomain_with_fat.py solver_study_resolution.py \
 --n_subdomains 2 1 3 \
 --multidomain_preconditioner_type $preconditioner \
---sampling_stride_x 1
---sampling_stride_y 1
---sampling_stride_z 32
---dt_0D             5e-4
---dt_multidomain    5e-4
---dt_splitting      5e-4
---scenario_name="s1_r12"
+--sampling_stride_x 1      \
+--sampling_stride_y 1      \
+--sampling_stride_z 32     \
+--dt_0D             5e-4   \
+--dt_multidomain    5e-4   \
+--dt_splitting      5e-4   \
+--scenario_name="s1$symmetric"   \
 $symmetric 
 
 killall -9 multidomain_shorten_with_fat; sleep 1
@@ -38,13 +38,13 @@ $example_dir/build_release/multidomain_shorten_with_fat \
 $example_dir/settings_multidomain_with_fat.py solver_study_resolution.py \
 --n_subdomains 4 1 6 \
 --multidomain_preconditioner_type $preconditioner \
---sampling_stride_x 1
---sampling_stride_y 1
---sampling_stride_z 16
---dt_0D             5e-4
---dt_multidomain    5e-4
---dt_splitting      5e-4
---scenario_name="s1_r24"
+--sampling_stride_x 1       \
+--sampling_stride_y 1       \
+--sampling_stride_z 16      \
+--dt_0D             5e-4    \
+--dt_multidomain    5e-4    \
+--dt_splitting      5e-4    \
+--scenario_name="s1$symmetric"    \
 $symmetric 
 
 killall -9 multidomain_shorten_with_fat; sleep 1
@@ -55,56 +55,15 @@ $example_dir/build_release/multidomain_shorten_with_fat \
 $example_dir/settings_multidomain_with_fat.py solver_study_resolution.py \
 --n_subdomains 6 1 8 \
 --multidomain_preconditioner_type $preconditioner \
---sampling_stride_x 1
---sampling_stride_y 1
---sampling_stride_z 8
---dt_0D             5e-4
---dt_multidomain    5e-4
---dt_splitting      5e-4
---scenario_name="s1_r48"
+--sampling_stride_x 1      \
+--sampling_stride_y 1      \
+--sampling_stride_z 8      \
+--dt_0D             5e-4   \
+--dt_multidomain    5e-4   \
+--dt_splitting      5e-4   \
+--scenario_name="s1$symmetric"   \
 $symmetric 
 
 killall -9 multidomain_shorten_with_fat; sleep 1
-
-# ------------
-# experiment 2
-# 2 ranks
-mpirun -n 2 \
-$example_dir/build_release/multidomain_shorten_with_fat \
-$example_dir/settings_multidomain_with_fat.py solver_study_resolution.py \
---n_subdomains 2 1 3 \
---multidomain_preconditioner_type $preconditioner \
---sampling_stride_x 2
---sampling_stride_y 2
---sampling_stride_z 32
---dt_0D             4e-4
---dt_multidomain    4e-4
---dt_splitting      4e-4
---scenario_name="s2_r2"
-$symmetric 
-
-killall -9 multidomain_shorten_with_fat; sleep 1
-
-# 64 ranks
-mpirun -n 64 \
-$example_dir/build_release/multidomain_shorten_with_fat \
-$example_dir/settings_multidomain_with_fat.py solver_study_resolution.py \
---n_subdomains 2 1 3 \
---multidomain_preconditioner_type $preconditioner \
---sampling_stride_x 1
---sampling_stride_y 1
---sampling_stride_z 16
---dt_0D             1e-4
---dt_multidomain    1e-4
---dt_splitting      1e-4
---scenario_name="s2_r64"
-$symmetric 
-
-killall -9 multidomain_shorten_with_fat; sleep 1
-
-# 2048 ranks
-
-
-
 done
 done
