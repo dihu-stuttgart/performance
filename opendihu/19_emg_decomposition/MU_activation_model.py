9de6380cc845d5581161f8965f0bb342d228cd00
Benjamin Maier <maier.bn@gmail.com>
2021-04-10 08:18:38 +0200

    Add output

--- opendihu/19_emg_decomposition/MU_activation_model.py
+++ opendihu/19_emg_decomposition/MU_activation_model.py
@@ -245,6 +245,30 @@ def raster_plot(label):
     y_predictions = model.predict(test_data_gen)
     np.savetxt('prediction.csv', y_predictions, delimiter=',')
     model.save('2040_IPT_MODEL.h5')
+    
+    # store ipt predictions
+    print(y_predictions.shape)
+    #    y_predictions = model.predict(test_data_gen)
+    #    np.savetxt('prediction.csv', y_predictions, delimiter=',')
+    #    model.save('2040_IPT_MODEL.h5')
+        # Kmeans clustering for converting to timestamp
+    predictions = KMeans(n_clusters=15, random_state=0).fit(y_predictions)
+    print("predictions: {}, shape: {}".format(predictions.labels_, predictions.labels_.shape))
+    IPT = []
+    n_classes = max(predictions.labels_)+1
+    print("n_classes: {}".format(n_classes))
+
+    with open('ipt_prediction.csv', 'wb') as f:
+      for i in range(n_classes):
+        if np.where(predictions.labels_ == i):
+          class_IPT = np.where(predictions.labels_ == i)
+          IPT.append(class_IPT)
+          np.savetxt(f, class_IPT, delimiter=';')
+    threshold_IPT = np.array(IPT).reshape(n_classes,-1)
+    print("IPT.shape: {}".format(threshold_IPT.shape))
+    
+    
+    
     predictions = np.argmax(np.array(y_predictions), axis=1)
     IPT = []
     for i in range(len(predictions)):
@@ -252,6 +276,7 @@ def raster_plot(label):
             IPT.append(np.where(predictions == i))
     predicted_IPT = np.array(IPT)
     IPT_pred = pd.DataFrame(predicted_IPT)
+    np.savetxt('predicted_IPT.csv', predicted_IPT, delimiter=',')
 
     b = label[len(y_train)+length:]
     vec = []
