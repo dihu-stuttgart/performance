3975fa4508db9c01382a3c8a1fe1570d3585314d
Benjamin Maier <maierbn@gmail.com>
2021-04-15 01:42:47 +0200

    Log

--- opendihu/22_solid_mechanics_vectorization/run.sh
+++ opendihu/22_solid_mechanics_vectorization/run.sh
@@ -8,12 +8,24 @@ example_dir=$OPENDIHU_HOME/examples/solid_mechanics/mooney_rivlin_transiso/
 input_dir=$OPENDIHU_HOME/examples/electrophysiology/input
 
 # run examples
-
+# weak scaling with approximately 8 elements per process
 for n_elements in \
-  "[1,1,1,1]" \
-  "[1,1,2,2]" \
-  "[2,1,2,2]" \
-  "[2,2,2,2]"
+  "[2,1,4,1]" \
+  "[2,2,4,2]" \
+  "[2,2,6,3]" \
+  "[2,2,8,4]" \
+  "[2,3,8,6]" \
+  "[3,4,8,12]" \
+  "[4,4,8,16]" \
+  "[3,4,16,24]" \
+  "[2,2,72,36]" \
+  "[2,2,108,54]" \
+  "[1,3,192,72]" \
+  "[4,4,48,96]" \
+  "[4,4,64,128]" \
+  "[2,4,144,144]" \
+  "[2,4,192,192]" \
+  "[2,4,256,256]";
 do
 
   # extract numbers of elements
@@ -21,17 +33,17 @@ do
   export ny=`python -c "print($n_elements[1])"`
   export nz=`python -c "print($n_elements[2])"`
   export nranks=`python -c "print($n_elements[3])"`
+  export oversubscribe=`python -c "print('--oversubscribe' if (int)($n_elements[3]) > 128 else '')"` 
 
   # run example with analytic jacobian, not vectorized
-  mpirun -n $nranks $example_dir/build_release/3d_hyperelasticity \
+  mpirun $oversubscribe -n $nranks $example_dir/build_release/3d_hyperelasticity \
     $example_dir/settings_3d_box.py --nx $nx --ny $ny --nz $nz \
-    --use_analytic_jacobian=True --use_numeric_jacobian=False --scenario_name="${nx}_${ny}_${nz}_analytic"
+    --use_analytic_jacobian=True --use_numeric_jacobian=False --scenario_name="analytic_jacobian" --file_output=false --njacobi=2
   
   # run example with numeric jacobian
-  echo mpirun -n $nranks $example_dir/build_release/3d_hyperelasticity \
+  mpirun $oversubscribe -n $nranks $example_dir/build_release/3d_hyperelasticity \
     $example_dir/settings_3d_box.py --nx $nx --ny $ny --nz $nz \
-    --use_analytic_jacobian=False --use_numeric_jacobian=True --scenario_name="${nx}_${ny}_${nz}_numeric"
+    --use_analytic_jacobian=False --use_numeric_jacobian=True --scenario_name="numeric_jacobian" --file_output=false --njacobi=2
     
-  break
 
 done
--- opendihu/22_solid_mechanics_vectorization/run2.sh
+++ opendihu/22_solid_mechanics_vectorization/run2.sh
@@ -12,26 +12,36 @@ input_dir=$OPENDIHU_HOME/examples/electrophysiology/input
 # run examples
 
 for n_elements in \
-  "[1,1,1,1]" \
-  "[1,1,2,2]" \
-  "[2,1,2,2]" \
-  "[2,2,2,2]"
+  "[4,2,8,1]" \
+  "[4,4,8,2]" \
+  "[4,4,12,3]" \
+  "[4,4,16,4]" \
+  "[4,6,16,6]" \
+  "[6,8,16,12]" \
+  "[8,8,16,16]" \
+  "[6,8,32,24]" \
+  "[2,9,128,36]" \
+  "[2,6,288,54]" \
+  "[8,8,96,96]" \
+  "[8,8,128,128]" \
+  "[4,8,288,144]" \
+  "[4,8,384,192]" \
+  "[4,8,512,256]"
 do
-
   # extract numbers of elements
   export nx=`python -c "print($n_elements[0])"`
   export ny=`python -c "print($n_elements[1])"`
   export nz=`python -c "print($n_elements[2])"`
   export nranks=`python -c "print($n_elements[3])"`
+  export oversubscribe=`python -c "print('--oversubscribe' if (int)($n_elements[3]) > 128 else '')"`
 
   # run example with analytic jacobian, not vectorized
-  mpirun -n $nranks $example_dir/build_release/3d_hyperelasticity_normal \
-    $example_dir/settings_3d_box.py --nx $nx --ny $ny --nz $nz --scenario_name="${nx}_${ny}_${nz}_not_vectorized"
+  mpirun $oversubscribe -n $nranks $example_dir/build_release/3d_hyperelasticity_normal \
+    $example_dir/settings_3d_box.py --nx $nx --ny $ny --nz $nz --scenario_name="not_vectorized" --file_output=False --njacobi=1
   
-  # run example with analytic jacobian
-  echo mpirun -n $nranks $example_dir/build_release/3d_hyperelasticity_vc \
-    $example_dir/settings_3d_box.py --nx $nx --ny $ny --nz $nz --scenario_name="${nx}_${ny}_${nz}_vectorized"
+  # run example with analytic jacobian, vectorized
+  mpirun $oversubscribe -n $nranks $example_dir/build_release/3d_hyperelasticity_vc \
+    $example_dir/settings_3d_box.py --nx $nx --ny $ny --nz $nz --scenario_name="vectorized" --file_output=False --njacobi=1
     
-  break
 
 done
